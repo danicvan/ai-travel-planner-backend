f0d1e20cfc42c2281abf48127d78640fea2f25ef
Daniel Evangelista <danicvan@hotmail.com>
2025-01-21 08:46:43 -0300

    feat: implement gpt-4o-mini chat and receive a answer

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-require("dotenv").config();
-const express = require("express");
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import OpenAi from "openai";
 
 const app = express();
 const port = process.env.PORT || 6000;
 
+const openai = new OpenAi({
+    apiKey: process.env.API_KEY,
+});
+
+const response = await openai.chat.completions.create({
+    messages: [{ role: "user", content: "Say this is a test" }],
+    model: "gpt-4o-mini"
+})
+
 app.listen(port, () => {
-    console.log(`Server is running on ${port} port.`);
+    console.log(JSON.stringify(`gpt-4o-mini answered and return request id: ${response._request_id}`));
 });

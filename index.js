da436bd80f31551aeec2b937248f777319088686
Daniel Evangelista <danicvan@hotmail.com>
2025-01-22 07:15:24 -0300

    feat: create api chat router to start to get answer to the gpt chat

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import OpenAi from "openai";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = process.env.PORT || 6000;
@@ -11,10 +12,22 @@ const openai = new OpenAi({
 });
 
 const response = await openai.chat.completions.create({
-    messages: [{ role: "user", content: "Say this is a test" }],
-    model: "gpt-4o-mini"
-})
+    model: "gpt-4o-mini",
+    messages: [
+        {
+            role: "user",
+            content: "Say this is a test"
+        }
+    ],
+});
+
+const jsonParser = bodyParser.json();
+const urlencodedParser = bordyParser.urlencoded({ extended: false });
+
+app.post('/api/chat', urlencodedParser, async (req, res) => {
+    console.log(`This receive a message: ${message}`);
+});
 
 app.listen(port, () => {
-    console.log(JSON.stringify(`gpt-4o-mini answered and return request id: ${response._request_id}`));
+    console.log(`Server is running on http://localhost:${port}.`);
 });

34d2ec5892e240edc511ae81a2fccf7df93f5412
Daniel Evangelista <danicvan@hotmail.com>
2025-01-22 09:12:54 -0300

    feat: up the server and contruct a api chat router

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import OpenAi from "openai";
-import bodyParser from "body-parser";
 
 const app = express();
 const port = process.env.PORT || 6000;
 
-const openai = new OpenAi({
-    apiKey: process.env.API_KEY,
-});
+express.json();
 
-const response = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
-    messages: [
-        {
-            role: "user",
-            content: "Say this is a test"
-        }
-    ],
-});
-
-app.post('/api/chat', (req, res) => {
-    res.send(`This is another test response.`);
+app.post('/api/chat', async (req, res) => {
+    req.send(res.json({ message: "Hello World!" }));
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}.`);
+    console.log(`This is my new server on http:localhost:${port}.`);
 });

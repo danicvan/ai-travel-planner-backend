194ca14fea58c8a40dff38ced310f29de3cc6965
Daniel Evangelista <danicvan@hotmail.com>
2025-01-23 07:23:47 -0300

    feat: import and install cors package for the system work

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import OpenAI from "openai";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 6000;
 
+app.use(cors());
 app.use(express.json());
 
 app.post('/api/chat', async (req, res) => {
